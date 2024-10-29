c27f3d2b892874a239be44bad96a0de3d99cdd87
tjdals4716 <song47166@naver.com>
2024-10-29 17:07:24 +0900

    :white_check_mark: 허용 포트 추가

--- src/main/java/com/example/moyeothon/Config/WebConfig.java
+++ src/main/java/com/example/moyeothon/Config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://hahasmile.netlify.app", "https://mango.angrak.cloud", "http://localhost:3000", "http://localhost:8080", "http://127.0.0.1:8080", "http://127.0.0.1:3000")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://127.0.0.1:8080", "http://127.0.0.1:3000", "http://localhost:5173", "http://127.0.0.1:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
