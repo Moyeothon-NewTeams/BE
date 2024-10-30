b15860cf97484d3695707ec15f84f205c9063cec
tjdals4716 <song47166@naver.com>
2024-10-30 11:48:35 +0900

    :white_check_mark: 스웨거 JWT 입력 헤더 추가

--- src/main/java/com/example/moyeothon/Config/Swagger/SwaggerConfig.java
+++ src/main/java/com/example/moyeothon/Config/Swagger/SwaggerConfig.java
@@ -1,9 +1,10 @@
 package com.example.moyeothon.Config.Swagger;
 
-
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,11 +12,21 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI(){
+    public OpenAPI openAPI() {
+        String securitySchemeName = "로그인 후 발급받은 JWT 적용하기";
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .info(apiInfo());
     }
+
     private Info apiInfo() {
         return new Info()
                 .title("\uD83E\uDD81 멋사 모여톤 10팀 스웨거 \uD83D\uDC8E")
