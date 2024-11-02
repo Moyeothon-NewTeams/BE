05ce463f875d6e66b1a823f72b99397a9ca7aad7
tjdals4716 <song47166@naver.com>
2024-11-02 22:20:48 +0900

    :recycle: cors 허용 도메인 원상복귀

--- src/main/java/com/example/moyeothon/Config/WebConfig.java
+++ src/main/java/com/example/moyeothon/Config/WebConfig.java
@@ -9,7 +9,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:5173", "https://newteamsgoody.shop", "https://moyeothon.vercel.app")
+                .allowedOrigins("http://localhost:8080",
+                                "http://127.0.0.1:8080",
+                                "http://127.0.0.1:3000",
+                                "http://localhost:5173",
+                                "http://127.0.0.1:5173",
+                                "https://newteamsgoody.shop",
+                                "https://moyeothon.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
