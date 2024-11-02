5da0d5fa4caf3be244f18d9d0d796443dd316ce3
tjdals4716 <song47166@naver.com>
2024-11-02 22:23:56 +0900

    :recycle: cors 허용 도메인 수정

--- src/main/java/com/example/moyeothon/Config/WebConfig.java
+++ src/main/java/com/example/moyeothon/Config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080",
                                 "http://127.0.0.1:8080",
+                                "http://localhost:3000",
                                 "http://127.0.0.1:3000",
                                 "http://localhost:5173",
                                 "http://127.0.0.1:5173",
